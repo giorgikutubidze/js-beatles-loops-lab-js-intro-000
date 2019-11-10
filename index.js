1d2acc9b1c4636f45720d3eca29f057f48b6582b
giorgi <giorgikutubidze@gmail.com>
2019-11-10 01:19:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // add solution here
+function theBeatlesPlay(arrofmus,arrofinst){
+  var emarray = [];
+  for(var i = 0;i < arrofmus.length;i++){
+    emarray.push(arrofmus[i] +' plays '+arrofinst[i]);
+  }
+  return emarray;
+}
+function johnLennonFacts(facts){
+var array1 = [];
+  let i = 0;
+  while(i < facts.length){
+    array1.push(facts[i] + '!!!');
+    i++;
+  }
+  return array1;
+}
